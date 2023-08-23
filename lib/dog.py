058ee3b999addf615650d9f55196a5d914fa9123
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-08-23 13:37:24 +0300

    Completed Task

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,48 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name, breed):
+        self._name = name
+        self._breed = breed
+
+    def get_name(self):
+        print('Retrieving name')
+        return self._name
+
+    def get_breed(self):
+        print("Retrieving Breed")
+        return  self._breed
+
+    def set_name(self, value):
+        if not isinstance(value, str):
+            print("Name must be a string.")
+        elif not (1 <= len(value) <= 25):
+            print("Name must be between 1 and 25 characters.")
+        else:
+            print(f"Setting name to: {value}")
+            self._name = value
+    name = property(get_name, set_name)
+
+    def set_breed(self, breed_name):
+        if breed_name not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            print(f"Setting breed to: {breed_name}")
+            self._breed = breed_name
+    breed = property(get_breed, set_breed)
+
+# Testing the Dog class
+guido = Dog("", 1)
+print(guido._name)
+guido.name = "Guido"
+print(guido._breed)
+guido.name = "Guido"
+guido.breed = "qwwqewew"
+print(guido.name)
+
+guido.name = 212
+guido.breed = "Pointer"
+
+
+
+
--- lib/person.py
+++ lib/person.py
@@ -14,6 +14,45 @@ APPROVED_JOBS = [
     "Marketing",
     "Purchasing"
 ]
+BREEDS=["Mastif", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "FRench Bulldog", "Pug", "Pointer"]
 
 class Person:
-    pass
+    def __init__(self, name="Grace", job="ITC"):
+        self._name = name
+        self._job = job
+
+    def get_name(self):
+        print("Retrieving name")
+        return self._name
+
+    def get_job(self):
+        print("Retrieving job")
+        return self._job
+
+    def set_name(self, name_value):
+        if not isinstance(name_value, str) and (1 <= len(str(name_value)) <25):
+            print( "Name must be string under 25 characters.")
+        else:
+            print(F"SETTING NAME TO {name_value}")
+            self._name = name_value.title()
+    name = property(get_name, set_name)
+
+    def set_job(self, job_name):
+        if job_name not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            print(f"Setting Job to: {job_name}")
+            self._job = job_name
+    job = property(get_job, set_job)
+
+
+
+p1 = Person("Arnoh","ITC")
+p1.name ="DEDEDEDEDEDEDE"
+print(p1.name)
+
+print("#################################")
+
+p1.job = "ITC"
+p1.job
+print(p1.job)
